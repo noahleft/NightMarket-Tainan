c41c939ebba24a2a7005fdc8067203df84fc1888
noahleft <noahleft@gmail.com>
2016-07-28 01:19:02 +0800

    initial commit

---
+++ NightMarket/NightMarketVO.swift
@@ -0,0 +1,47 @@
+//
+//  NightMarketVO.swift
+//  NightMarket
+//
+//  Created by lin shih-feng on 7/28/16.
+//  Copyright © 2016 lin shih-feng. All rights reserved.
+//
+
+import Foundation
+
+class NightMarketVO {
+    
+    var locationName: String?
+    var latitude: Double?
+    var longitude: Double?
+    var locationDescription: String?
+    var type: String?
+    var openingTime: String?
+    var size: Int?
+    
+    
+    init(aLocationName:String,
+         aLatitude:Double,
+         aLongitude:Double,
+         aLocationDescription:String,
+         aType:String,
+         aOpeningTime:String,
+         aSize:Int) {
+        self.locationName = aLocationName
+        self.latitude = aLatitude
+        self.longitude = aLongitude
+        self.locationDescription = aLocationDescription
+        self.type = aType
+        self.openingTime = aOpeningTime
+        self.size = aSize
+    }
+    
+    var description:String{
+        let _result:String = "NightMarketVO" + " LocationName\(self.locationName)"
+                           + ",Latitude\(self.latitude)" + ",Longitude\(self.longitude)"
+                           + ",LocationDescription\(self.locationDescription)" + ",Type\(self.type)"
+                           + ",OpeningTime\(self.openingTime)" + ",Size\(self.size)"
+        return _result
+    }
+    
+}
+
