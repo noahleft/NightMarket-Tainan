0bae41684046de2481f17195c270e55dde694192
noahleft <noahleft@gmail.com>
2016-08-15 23:29:28 +0800

    initial commit

---
+++ NightMarket/LocationTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  LocationTableViewCell.swift
+//  NightMarket
+//
+//  Created by lin shih-feng on 06/08/2016.
+//  Copyright © 2016 lin shih-feng. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LocationTableViewCell: UITableViewCell {
+    
+    @IBOutlet var LocationNameLabel: UILabel!
+    @IBOutlet var LocationDescriptionLabel: UILabel!
+    
+}
